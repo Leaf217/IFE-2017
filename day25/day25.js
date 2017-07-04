d72d778f58aa55e5fb9624fd04bad11670d6a394
Leaf217 <leaf_217@163.com>
2017-07-04 09:01:49 +0800

    version4

--- day25/day25.js
+++ day25/day25.js
@@ -8,17 +8,18 @@ window.onload = function () {
     var tabCon = document.getElementById('tab-con');
     var tabNavi = document.getElementById('tab-navi');
     var normalTr = tabCon.getElementsByTagName('tr');
-    var fixedTr = document.getElementById('fixed');
-    var availWidth = screen.availWidth;
     var availHeight = screen.availHeight;
     var rectNavi = tabNavi.getBoundingClientRect();//动态获取ul元素到视窗的距离，这是一个集合，有上下左右等的距离
     var rectCon = tabCon.getBoundingClientRect();
+    var fixedTab = document.getElementById('tab-fixed');
 
 
     //给表格添加背景色
     for (var i = 2;i < normalTr.length;i += 2) {
         normalTr[i].className = 'grey';
     }
+
+
     addEvent(window, 'mousewheel', winScroll);
     addEvent(window, 'DOMMouseScroll', winScroll);
 
@@ -31,25 +32,18 @@ window.onload = function () {
      * 当滚动条离初始位置的距离 < 表格顶部离页面最顶部的距离时，表格标题正常显示，否则，将固定
      */
     function winScroll(){
-        if (document.body.scrollTop < rectCon.top) {
+        if (getScrollXY().y < rectCon.top) {
             tab.style.position = '';
-            fixedTr.style.position = '';
-            fixedTr.style.top = '';
+            fixedTab.style.display = 'none';
             tabCon.style.position = '';
             tabCon.style.top = '';
             tabNavi.style.position = '';
             tabNavi.style.top = '';
-            tabNavi.style.height = availHeight - rectNavi.top - 135 + document.body.scrollTop + 'px';//availHeight - rectNavi就是tabnavi应该显示的高度
+            tabNavi.style.height = innerHeight - rectNavi.top + getScrollXY().y + 'px';//availHeight - rectNavi就是tabnavi应该显示的高度
             tabNavi.style.overflow = 'scroll';
-        } else if (document.body.scrollTop >= rectCon.top) {
-            tabNavi.style.height = availHeight -135 + 'px';
-            tab.style.position = 'relative';
-            tabCon.style.position = 'absolute';
-            tabCon.style.top = '50px';
-            // fixedTr.style.width =  rectCon.right - rectCon.left + 'px';
-            // fixedTr.style.width = availWidth - rectCon.left - (availWidth - rectCon.right) + 'px';
-            fixedTr.style.position = 'fixed';
-            fixedTr.style.top = '0';
+        } else if (getScrollXY().y >= rectCon.top) {
+            tabNavi.style.height = innerHeight + 'px';
+            fixedTab.style.display = 'block';
             tabNavi.style.position = 'fixed';
             tabNavi.style.top = '0';
         }
@@ -57,7 +51,7 @@ window.onload = function () {
 
 
     /**
-     * 绑定时间函数，为了浏览器的兼容而生
+     * 绑定事件函数，解决浏览器兼容问题
      * @param obj 元素名称
      * @param xEvent 事件名称
      * @param fn 函数名称
@@ -68,6 +62,26 @@ window.onload = function () {
         }else{
             obj.addEventListener(xEvent,fn,false);
         }
+    }
+
+
+    /**
+     * 解决scrillTop火狐浏览器的兼容问题
+     * getScrollXY.y = scrollTop
+     * getScrollXY.x = scrollLeft
+     * @returns {{x: *, y: *}}
+     */
+    function getScrollXY() {
+        var x, y;
+        if (document.body.scrollTop) { //非标准写法,chrome能识别
+            x = document.body.scrollLeft;
+            y = document.body.scrollTop;
+        }
+        else { //符合标准规范的标准写法
+            x = document.documentElement.scrollLeft;
+            y = document.documentElement.scrollTop;
+        }
+        return {x: x, y: y};
     }
 
 }
