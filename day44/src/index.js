c29938c5c4d3bcc35db8322dae1778ff08b272a1
Leaf217 <leaf_217@163.com>
2017-07-25 18:29:58 +0800

    day44 v1

--- day44/src/index.js
+++ day44/src/index.js
@@ -1,17 +1,28 @@
 /**
  * Created by zhu_yeqing on 2017/7/23.
  */
-import _ from 'lodash';
+// import _ from 'lodash';
 var style = require('./css/main.less');
 
-// import "main.less";
-function component() {
-    var element = document.createElement('div');
+function basic() {
+    var elem = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.id = 'hello';
+    elem.id = 'elem';
+    elem.innerHTML = '<div id="top">'
+                   + '<p>Title</p> '
+                   + '<button type="button">Add</button>'
+                   + '</div>'
 
-    return element;
+                   + '<div id="priority">'
+                   + '<div class="flex"><div id="red-circle"></div> '
+                   + '<div class="text">高优</div></div>'
+                   + '<div class="flex"><div id="yellow-circle"></div> '
+                   + '<div class="text">中优</div></div> '
+                   + '<div class="flex"><div id="green-circle"></div> '
+                   + '<div class="text">低优</div></div>'
+                   + '</div>';
+    return elem;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(basic());
+
